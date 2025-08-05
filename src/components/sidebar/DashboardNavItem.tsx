fe762409121d4d21f1b94936d80598928f5a4128
Joan Manrubia <jmanrumartinez@gmail.com>
2025-08-05 11:31:48 -0400

    feat: replace anchor tag with Next.js Link component in DashboardNavItem for improved navigation

--- src/components/sidebar/DashboardNavItem.tsx
+++ src/components/sidebar/DashboardNavItem.tsx
@@ -1,5 +1,6 @@
 import { LucideIcon } from "lucide-react";
 import { SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
+import Link from "next/link";
 
 export type DashboardNavItemType = {
   title: string;
@@ -12,12 +13,12 @@ export const DashboardNavItem = ({ item }: { item: DashboardNavItemType }) => {
   return (
     <SidebarMenuItem key={item.title}>
       <SidebarMenuButton asChild isActive={item.isActive}>
-        <a href={item.url} className="flex items-center justify-between">
+        <Link href={item.url} className="flex items-center justify-between">
           <div className="flex items-center">
             <item.icon className="mr-3 h-4 w-4" />
             <span>{item.title}</span>
           </div>
-        </a>
+        </Link>
       </SidebarMenuButton>
     </SidebarMenuItem>
   );
