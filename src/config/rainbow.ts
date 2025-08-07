7f43e42704ebe0595e07b67fdd86cd0618d2c818
Joan Manrubia <jmanrumartinez@gmail.com>
2025-08-07 10:40:43 -0400

    feat: add base chain to configuration for enhanced multi-chain support

--- src/config/rainbow.ts
+++ src/config/rainbow.ts
@@ -1,11 +1,11 @@
 "use client";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet } from "viem/chains";
+import { base, mainnet } from "viem/chains";
 
 const config = getDefaultConfig({
   appName: "DeFi Dashboard",
   projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID ?? "",
-  chains: [mainnet],
+  chains: [mainnet, base],
   ssr: true,
 });
 
